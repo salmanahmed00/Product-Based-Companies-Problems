5a7b0f39b4cc8c12b7dbd3cfcca206ce7d02500c
Salman Ahmed <ahmedpec1005@gmail.com>
2022-06-27 15:07:03 +0530

    Create flipBits.cpp

---
+++ Array/flipBits.cpp
@@ -0,0 +1,10 @@
+/*
+***Flip Bits***
+
+Link: https://www.codingninjas.com/codestudio/guided-paths/interview-guide-for-product-based-companies/content/110297/offering/1280149
+
+You are given an array of integers ARR[] of size N consisting of zeros and ones. You have to select a subset and flip bits of that subset. 
+You have to return the count of maximum one’s that you can obtain by flipping chosen sub-array at most once.
+A flip operation is one in which you turn 1 into 0 and 0 into 1.
+
+*/
